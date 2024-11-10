eb32a025d996120095722cc6805192278d37d871
nikhi <nikhi@host.docker.internal>
2024-11-10 13:54:51 +0530

    Initial Commit

---
+++ SampleMavenProj01/src/main/java/Nit/SampleMavenProj01/App.java
@@ -0,0 +1,10 @@
+package Nit.SampleMavenProj01;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SampleMavenProj01/src/main/java/com/nt/AE/Arithmeticoperations.java
@@ -0,0 +1,16 @@
+package com.nt.AE;
+
+public class Arithmeticoperations 
+{
+   public int add(int x,int y)
+   {
+	   return x+y;
+   }
+   
+   public static void main(String[] args) 
+   {
+	  Arithmeticoperations aobj=new Arithmeticoperations();
+	  System.out.println(aobj.add(25,75));
+   }
+   
+}
